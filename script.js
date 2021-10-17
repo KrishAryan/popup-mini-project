ade0edc47e92a298bb4992805912c40588f76fb8
KrishAryan <krisharyan.36@gmail.com>
2021-10-18 01:05:44 +0530

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log("hello")
-const y= document.querySelectorAll(".popup")
-console.log(y.length)
+const popups= document.querySelectorAll(".popup");
+const popup=document.querySelector(".output");
+const popMessage=document.querySelector(".message");
+const closeButton=document.querySelector(".close")
+
+closeButton.addEventListener("click",function(){
+  popup.classList.add("hide");
+})
+for (let x=0; x<popups.length; x++)
+{
+  popups[x].addEventListener("click", function(){
+  let outputText = this.getAttribute("data-message")
+  message(outputText)
+  })  
+}
+function message(output){
+  popup.classList.remove("hide");
+  popMessage.innerHTML = output;
+}
